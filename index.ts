a6d2a7fedef224874a3b9f17d6216accd4422d4e
Kane Longbottom <longbottomk@outlook.com>
2022-01-09 18:02:47 +0000

    format change

--- index.ts
+++ index.ts
@@ -19,9 +19,7 @@ const startServer = async () => {
     });
     await server.start();
     server.applyMiddleware({ app });
-    app.listen({ port: process.env.PORT }, () =>
-        console.log(`Server running on port ${process.env.PORT}`),
-    );
+    app.listen({ port: process.env.PORT }, () => console.log(`Server running on port ${process.env.PORT}`));
 };
 
 startServer();
